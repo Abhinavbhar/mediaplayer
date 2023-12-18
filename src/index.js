9a6148c7e89690d4afba7a771102f6809cb4f192
abhinav <abhinavbhar2004@gmail.com>
2023-12-18 22:03:46 +0530

    connected to mongotlass (mongoose)

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import express from 'express';
+import connectDB from './db/index.js';
+dotenv.config({
+  path: './env'
+})
+
+const app = express();
+/*(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+    app.listen(process.env.PORT, () => {
+      console.log("App is running on port ", process.env.PORT);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+})(); */
+connectDB();
